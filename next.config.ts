b368cb6e90d8477e5f91a8f845919ad10f040d21
Monark Basak <monarkb01@gmail.com>
2024-12-11 00:09:15 -0500

    Needed these changes to get react-pdf to work

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
+import { pdfjs } from "react-pdf";
+
+/* pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.mjs",
+  import.meta.url
+).toString(); */
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    turbo: {
+      resolveAlias: {
+        canvas: "./empty-module.ts",
+      },
+    },
+  },
 };
 
 export default nextConfig;
